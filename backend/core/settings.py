5ed81aca5db0278e155f70fe1c493d8359b0fb33
Hyrozdmr <hayriozdemir@hotmail.co.uk>
2024-11-11 14:25:31 +0000

    Update .gitignore and settings

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -9,18 +9,30 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Load environment variables from .env file
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xryd3^d296_(=fq2%#vv2!p@^a)z4=ra33!arjd1-cfyjwj-fw'
+SECRET_KEY = os.getenv('SECRET_KEY')
+TMDB_API_KEY = os.getenv('TMDB_API_KEY')
+
+
+if not SECRET_KEY:
+    raise ValueError("SECRET_KEY is missing from environment variables")
+
+if not TMDB_API_KEY:
+    raise ValueError("TMDB_API_KEY is missing from environment variables")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,9 +49,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+    'rest_framework',
+    'corsheaders',
+    'apps.movies',
+    'apps.users',
+    'apps.admin_dashboard',
+    ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -48,6 +66,15 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
 
 ROOT_URLCONF = 'core.urls'
 
