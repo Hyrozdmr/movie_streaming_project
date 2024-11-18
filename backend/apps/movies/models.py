9e87e779b71d1df5a9a7c03e48d5ffd9e492cec1
Hyrozdmr <hayriozdemir@hotmail.co.uk>
2024-11-18 15:05:16 +0000

    movie and tv-show related database schemas

--- backend/apps/movies/models.py
+++ backend/apps/movies/models.py
@@ -1,13 +1,30 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Movie(models.Model):
+    tmdb_id = models.IntegerField(unique=True)
+    title = models.CharField(max_length=255)
+    overview = models.TextField()
+    poster_path = models.CharField(max_length=255)
+    backdrop_path = models.CharField(max_length=255)
+    release_date = models.DateField()
+    vote_average = models.FloatField()
+    vote_count = models.IntegerField()
+    media_type = models.CharField(max_length=10, default='movie')
+
+class TVShow(models.Model):
+    tmdb_id = models.IntegerField(unique=True)
+    title = models.CharField(max_length=255)
+    overview = models.TextField()
+    poster_path = models.CharField(max_length=255)
+    backdrop_path = models.CharField(max_length=255)
+    first_air_date = models.DateField()
+    vote_average = models.FloatField()
+    vote_count = models.IntegerField()
+    media_type = models.CharField(max_length=10, default='tv')
+
 class WatchList(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    movie_id = models.IntegerField()
-    title = models.CharField(max_length=255)
-    poster_path = models.CharField(max_length=255)
-    media_type = models.CharField(max_length=10)  # 'movie' or 'tv'
+    media = models.ForeignKey(Movie, on_delete=models.CASCADE, null=True)
+    media_type = models.CharField(max_length=10)
     added_date = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        unique_together = ('user', 'movie_id', 'media_type')
