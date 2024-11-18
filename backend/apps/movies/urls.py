1859232798ac78d1e865c3b0a7512ee049a3abad
Hyrozdmr <hayriozdemir@hotmail.co.uk>
2024-11-18 15:06:54 +0000

    app-specific routes with views

--- backend/apps/movies/urls.py
+++ backend/apps/movies/urls.py
@@ -3,10 +3,14 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 router = DefaultRouter()
+router.register(r'movies', views.MovieViewSet)
+router.register(r'tv-shows', views.TVShowViewSet)
 router.register(r'watchlist', views.WatchListViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('movies/<str:category>/', views.fetch_movies),
+    path('popular/', views.fetch_popular_media),
+    path('now-playing/', views.fetch_now_playing_media),
+    path('upcoming/', views.fetch_upcoming_media),
     path('search/', views.search_media),
 ]
