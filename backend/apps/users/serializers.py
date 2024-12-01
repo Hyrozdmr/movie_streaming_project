aee1a9ffc4d0a4c5d431cd95e20c6d5840846929
Hyrozdmr <hayriozdemir@hotmail.co.uk>
2024-12-01 14:25:03 +0000

    Add user serializer

--- backend/apps/users/serializers.py
+++ backend/apps/users/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import User
-from movies.serializers import MovieSerializer, TVShowSerializer
+from apps.movies.serializers import MovieSerializer, TVShowSerializer
 
 class UserSerializer(serializers.ModelSerializer):
     watchlist = MovieSerializer(many=True, read_only=True)
