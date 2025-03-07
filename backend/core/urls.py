e83b729b4f1e8b6d859019daadae9bb02475424d
Hyrozdmr <hayriozdemir@hotmail.co.uk>
2025-03-07 23:29:18 +0000

    route update

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,27 +1,29 @@
-"""
-URL configuration for core project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+# Define a root API endpoint
+@api_view(['GET'])
+def api_root(request):
+    return Response({
+        "movies": "/api/movies/",
+        "tvshows": "/api/tvshows/",
+        "watchlist": "/api/watchlist/",
+        "search": "/api/search/",
+        "users": "/api/users/",
+        "token": "/api/token/",
+        "token_refresh": "/api/token/refresh/",
+    })
 
 urlpatterns = [
+    path('', api_root, name='api-root'),  # Root API page
     path('admin/', admin.site.urls),
-    path('api/movies/', include('apps.movies.urls')),
-    path('api/users/', include('apps.users.urls')),
-    # path('api/admin/', include('apps.admin_dashboard.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('api/', include('apps.movies.urls')),
+    path('api/', include('apps.users.urls')),
+
+    # JWT Authentication Endpoints
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
